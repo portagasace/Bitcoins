44bf77fef29ad53fce9108e4a0254190a4e77750
Aaditya Prakash <72169934+portagasace@users.noreply.github.com>
2021-09-18 15:16:08 -0400

    Readme added

--- Program.fs
+++ Program.fs
@@ -7,7 +7,6 @@ open Akka.FSharp
 open Akka.TestKit
 open Microsoft.FSharp.Core
 
-
 // Generating a Random String
 let ranStr n = 
     let r = Random()
@@ -15,29 +14,33 @@ let ranStr n =
     let sz = Array.length chars in
     String(Array.init n (fun _ -> chars.[r.Next sz]))
     
-
 // Computing Hash
 let SHA256 (text:string) =
-   
-    use sha256Hash = SHA256Managed.Create()
-    sha256Hash.ComputeHash(System.Text.Encoding.ASCII.GetBytes(text))
+    System.Text.Encoding.ASCII.GetBytes(text)
+    |>(new SHA256Managed()).ComputeHash 
+    |> Array.map (fun (x : byte) -> System.String.Format("{0:X2}", x))
+    |> String.concat System.String.Empty
 
 
-let string1 = "aprakash" + ranStr(30)
-printfn $"Random String is: %s{string1}"
 
-let hash = System.Text.Encoding.ASCII.GetString(SHA256(string1))
-printfn $"Hash is: %s{hash}"
+let randomString = "aprakash" + ranStr(30)
+let hashString = SHA256(randomString)
+
+printfn $"Random String is: %s{randomString}"
+printfn $"Hash is: %s{hashString}"
 
 // Coverting Hash to a list
-let n = System.Console.ReadLine()
-let substring = hash.[0..n]
-hash |> Seq.iter (fun x->printfn "%c" x)
+//let r = System.Console.ReadLine();
+       //Some(int(n))
+
+//let n = r
+//let substring = hash.[0..n]
+//hash |> Seq.iter (fun x->printfn "%c" x)
 
 
-for i = 1 to n do
-    if (x.[i]==0) then count++        
-    else
+//for i = 1 to n do
+    //if (x.[i]==0) then count++        
+    //else
 
 //
 
