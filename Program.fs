5ad0e98b157e18276ac61964f9955b93112dd9c6
Aaditya Prakash <72169934+portagasace@users.noreply.github.com>
2021-09-18 13:30:38 -0400

    Hash to list

--- Program.fs
+++ Program.fs
@@ -5,10 +5,10 @@ open Akka.Actor
 open Akka.Configuration
 open Akka.FSharp
 open Akka.TestKit
+open Microsoft.FSharp.Core
 
-// #Simplify Actor
-// There is a simpler way to define an Actor
 
+// Generating a Random String
 let ranStr n = 
     let r = Random()
     let chars = Array.concat([[|'a' .. 'z'|];[|'A' .. 'Z'|];[|'0' .. '9'|]])
@@ -16,11 +16,12 @@ let ranStr n =
     String(Array.init n (fun _ -> chars.[r.Next sz]))
     
 
-
+// Computing Hash
 let SHA256 (text:string) =
    
     use sha256Hash = SHA256Managed.Create()
     sha256Hash.ComputeHash(System.Text.Encoding.ASCII.GetBytes(text))
+
 
 let string1 = "aprakash" + ranStr(30)
 printfn $"Random String is: %s{string1}"
@@ -28,9 +29,19 @@ printfn $"Random String is: %s{string1}"
 let hash = System.Text.Encoding.ASCII.GetString(SHA256(string1))
 printfn $"Hash is: %s{hash}"
 
-
-
+// Coverting Hash to a list
 let n = System.Console.ReadLine()
+let substring = hash.[0..n]
+hash |> Seq.iter (fun x->printfn "%c" x)
+
+
+for i = 1 to n do
+    if (x.[i]==0) then count++        
+    else
+
+//
+
+//while (n < 20) do
 
 
 
